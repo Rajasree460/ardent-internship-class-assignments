85b98fbbf6c31d80f97d605faa30d2d1ab0538f4
Rajasree Laha <laharajasree121@gmail.com>
2024-07-05 16:55:32 +0530

    Create App_ass2.js

---
+++ App_ass2.js
@@ -0,0 +1,22 @@
+ import React from 'react';
+ import './App.css';
+ import First from './components/First';
+ import Second from './components/First';
+ import Third from './components/First';
+ import Fourth from './components/First';
+ import Fifth from './components/First';
+
+function App() {
+   return (
+     <div className="App">
+       <First/>
+       <Second/>
+       <Third/>
+       <Fourth/>
+       <Fifth/>
+     </div>
+   );
+ }
+
+
+ export default App;

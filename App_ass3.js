80196948747f6498d46e161faa4380b38517559a
Rajasree Laha <laharajasree121@gmail.com>
2024-07-05 17:18:17 +0530

    Update App_ass3.js

--- App_ass3.js
+++ App_ass3.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import {First,Second,Third,Fourth,Fifth} from './components/First';
+
 
 function App() {
   return (
@@ -13,54 +15,8 @@ function App() {
   );
 }
 
-function First() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-   </div>
-  )
-  
-}
-
-
-function Second() {
-  return (
-    <div>
-      <h2>Hello World</h2>
-   </div>
-  )
-  
-}
-
-
-function Third() {
-  return (
-    <div>
-      <h3>Hello World</h3>
-   </div>
-  )
-  
-}
-
-
-
-function Fourth() {
-  return (
-    <div>
-      <h4>Hello World</h4>
-   </div>
-  )
-  
-}
-
-
-function Fifth() {
-  return (
-    <div>
-      <h5>Hello World</h5>
-   </div>
-  )
-  
-}
 
 export default App;
+
+
+

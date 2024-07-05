c62c0d7ba884bcf2dd9d1a210a0dd42fa2b2d6ad
Rajasree Laha <laharajasree121@gmail.com>
2024-07-05 17:56:47 +0530

    Create First_class.jsx

---
+++ First_class.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+class Firstc extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello class Component</h1>
+            </div>
+        )
+    }
+}
+export default Firstc

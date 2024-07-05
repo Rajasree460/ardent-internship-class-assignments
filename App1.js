b197639e80f4f5d660008a243d636f37cb6c79a0
Rajasree Laha <laharajasree121@gmail.com>
2024-07-05 16:43:45 +0530

    Create App1.js

---
+++ App1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import First from './components/First';
+
+function App() {
+  return (
+    <div className="App">
+      <First/>
+    </div>
+  );
+}
+
+
+export default App;

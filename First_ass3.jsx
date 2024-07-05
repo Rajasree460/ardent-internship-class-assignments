d4a5b7ab2b0c35ba6f7bd1171533494a3e93f2ae
Rajasree Laha <laharajasree121@gmail.com>
2024-07-05 17:19:02 +0530

    Create First_ass3.jsx

---
+++ First_ass3.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+const First=()=>{
+    return (
+        <>
+        <h1>Hello I am Rajasree</h1>
+        </>
+    )
+}
+
+const Second=()=>{
+    return (
+        <>
+        <h1>Hello I am Rajasree</h1>
+        </>
+    )
+}
+
+const Third=()=>{
+    return (
+        <>
+        <h1>Hello I am Rajasree</h1>
+        </>
+    )
+}
+
+const Fourth=()=>{
+    return (
+        <>
+        <h1>Hello I am Rajasree</h1>
+        </>
+    )
+}
+
+const Fifth=()=>{
+    return (
+        <>
+        <h1>Hello I am Rajasree</h1>
+        </>
+    )
+}
+export  {First,Second,Third,Fourth,Fifth}

b89f57223db7b29f88b49bfd90283d0a31cc4da3
Rajasree Laha <laharajasree121@gmail.com>
2024-07-05 17:56:08 +0530

    Create App_class_comp.jsx

---
+++ App_class_comp.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Firstc from './components/Firstc';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Firstc/>
+    </div>
+  );
+}
+
+
+export default App;
+

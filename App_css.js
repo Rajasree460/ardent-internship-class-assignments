3128260cafa85a4ee93d3f943d9246e62be4366a
Rajasree Laha <laharajasree121@gmail.com>
2024-07-08 18:15:51 +0530

    Create App_css.js

---
+++ App_css.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{color:'red',background:'yellow'}}>Hello component</h1>
+      <h1 style={{color:'rgb(0,100,0)'}}>Hello Component</h1>
+      <h1 style={{color:'#FF0080',background:'yellow',textAlign:'center',fontSize:'50px'}}>Hello Component</h1>
+      <h1 style={{color:'#FF0080', backgroundColor:'green', textAlign:'center', fontSize:'50px', fontFamily:'Margarine'}}>Hello Component</h1>
+    </div>
+  );
+}
+
+
+export default App;
+

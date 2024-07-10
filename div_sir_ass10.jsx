a37eb2e82d3b090bf5e5b9797a98f6efe9e29350
Rajasree Laha <laharajasree121@gmail.com>
2024-07-10 16:30:39 +0530

    Create div_sir_ass10.jsx

---
+++ div_sir_ass10.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import './css/Mycss.css';
+
+
+const Division = () =>{
+    return(
+        <>
+            <div className="first">
+
+            </div>
+            <div className="sec">
+
+
+            </div>
+            <div className="third">
+
+
+            </div>
+            <div className="fourth">
+
+
+            </div>
+            <div className="fifth">
+
+
+            </div>
+            <div className="sixth">
+
+
+            </div>
+            
+            <div className="seventh">
+
+
+            </div>
+            <div className="eighth">
+
+
+            </div>
+            <div className="nineth">
+
+
+            </div>
+            <div className="tenth">
+
+
+            </div>
+            <div className="eleventh">
+
+
+            </div>
+            <div className="twelveth">
+
+
+            </div>
+            <div className="thir">
+
+
+            </div>
+            <div className="four">
+
+
+            </div>
+
+            <div className="fif">
+
+
+            </div>
+            <div className="six">
+
+
+            </div>
+            <div className="sev">
+
+
+            </div>
+            <div className="eig">
+
+
+            </div>
+            <div className="nine">
+
+
+            </div>
+            <div className="twen">
+
+
+            </div>
+        </>
+    )
+}
+
+
+export default Division

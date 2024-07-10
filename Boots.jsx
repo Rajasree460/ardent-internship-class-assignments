8f8ac7faae6e07dded254ff2d66cf56878b83eb0
Rajasree Laha <laharajasree121@gmail.com>
2024-07-10 17:55:30 +0530

    Create Boots.jsx

---
+++ Boots.jsx
@@ -0,0 +1,137 @@
+import React from "react";
+const burd = 'img/earb.jpg';
+
+
+const Boots = () =>{
+    return(
+        <>
+            <div className="container-fluid mt-5">
+                <div className="row">
+                    <div className="col-lg-2 col-md-4 col-sm-12">
+                        <div className="card">
+                            <img src={burd} alt={burd} className="img-fluid"></img>
+                            <h6 className="text-center" style={{marginTop:'-10px'}}>Fancy Earburds</h6>
+                            <span style={{textAlign:'center', display:'block', color:'#40A578', marginTop:'-5px'}}>This Is Good Product</span>
+                            <span style={{textAlign:'center', display:'block'}}>Rs.1500.00 <span style={{color:'red', textDecoration:'line-through'}}>Rs.5000.00</span></span>
+                        </div>
+                    </div>
+
+
+                    <div className="col-lg-2 col-md-4 col-sm-12">
+                        <div className="card">
+                            <img src={burd} alt={burd} className="img-fluid"></img>
+                            <h6 className="text-center" style={{marginTop:'-10px'}}>Fancy Earburds</h6>
+                            <span style={{textAlign:'center', display:'block', color:'#40A578', marginTop:'-5px'}}>This Is Good Product</span>
+                            <span style={{textAlign:'center', display:'block'}}>Rs.1500.00 <span style={{color:'red', textDecoration:'line-through'}}>Rs.5000.00</span></span>
+                        </div>
+                    </div>
+
+
+                    <div className="col-lg-2 col-md-4 col-sm-12">
+                        <div className="card">
+                            <img src={burd} alt={burd} className="img-fluid"></img>
+                            <h6 className="text-center" style={{marginTop:'-10px'}}>Fancy Earburds</h6>
+                            <span style={{textAlign:'center', display:'block', color:'#40A578', marginTop:'-5px'}}>This Is Good Product</span>
+                            <span style={{textAlign:'center', display:'block'}}>Rs.1500.00 <span style={{color:'red', textDecoration:'line-through'}}>Rs.5000.00</span></span>
+                        </div>
+                    </div>
+
+
+                    <div className="col-lg-2 col-md-4 col-sm-12">
+                        <div className="card">
+                            <img src={burd} alt={burd} className="img-fluid"></img>
+                            <h6 className="text-center" style={{marginTop:'-10px'}}>Fancy Earburds</h6>
+                            <span style={{textAlign:'center', display:'block', color:'#40A578', marginTop:'-5px'}}>This Is Good Product</span>
+                            <span style={{textAlign:'center', display:'block'}}>Rs.1500.00 <span style={{color:'red', textDecoration:'line-through'}}>Rs.5000.00</span></span>
+                        </div>
+                    </div>
+
+
+                    <div className="col-lg-2 col-md-4 col-sm-12">
+                        <div className="card">
+                            <img src={burd} alt={burd} className="img-fluid"></img>
+                            <h6 className="text-center" style={{marginTop:'-10px'}}>Fancy Earburds</h6>
+                            <span style={{textAlign:'center', display:'block', color:'#40A578', marginTop:'-5px'}}>This Is Good Product</span>
+                            <span style={{textAlign:'center', display:'block'}}>Rs.1500.00 <span style={{color:'red', textDecoration:'line-through'}}>Rs.5000.00</span></span>
+                        </div>
+                    </div>
+
+
+                    <div className="col-lg-2 col-md-4 col-sm-12">
+                        <div className="card">
+                            <img src={burd} alt={burd} className="img-fluid"></img>
+                            <h6 className="text-center" style={{marginTop:'-10px'}}>Fancy Earburds</h6>
+                            <span style={{textAlign:'center', display:'block', color:'#40A578', marginTop:'-5px'}}>This Is Good Product</span>
+                            <span style={{textAlign:'center', display:'block'}}>Rs.1500.00 <span style={{color:'red', textDecoration:'line-through'}}>Rs.5000.00</span></span>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="row mt-3">
+                    <div className="col-lg-2 col-md-4 col-sm-12">
+                        <div className="card">
+                            <img src={burd} alt={burd} className="img-fluid"></img>
+                            <h6 className="text-center" style={{marginTop:'-10px'}}>Fancy Earburds</h6>
+                            <span style={{textAlign:'center', display:'block', color:'#40A578', marginTop:'-5px'}}>This Is Good Product</span>
+                            <span style={{textAlign:'center', display:'block'}}>Rs.1500.00 <span style={{color:'red', textDecoration:'line-through'}}>Rs.5000.00</span></span>
+                        </div>
+                    </div>
+
+
+                    <div className="col-lg-2 col-md-4 col-sm-12">
+                        <div className="card">
+                            <img src={burd} alt={burd} className="img-fluid"></img>
+                            <h6 className="text-center" style={{marginTop:'-10px'}}>Fancy Earburds</h6>
+                            <span style={{textAlign:'center', display:'block', color:'#40A578', marginTop:'-5px'}}>This Is Good Product</span>
+                            <span style={{textAlign:'center', display:'block'}}>Rs.1500.00 <span style={{color:'red', textDecoration:'line-through'}}>Rs.5000.00</span></span>
+                        </div>
+                    </div>
+
+
+                    <div className="col-lg-2 col-md-4 col-sm-12">
+                        <div className="card">
+                            <img src={burd} alt={burd} className="img-fluid"></img>
+                            <h6 className="text-center" style={{marginTop:'-10px'}}>Fancy Earburds</h6>
+                            <span style={{textAlign:'center', display:'block', color:'#40A578', marginTop:'-5px'}}>This Is Good Product</span>
+                            <span style={{textAlign:'center', display:'block'}}>Rs.1500.00 <span style={{color:'red', textDecoration:'line-through'}}>Rs.5000.00</span></span>
+                        </div>
+                    </div>
+
+
+                    <div className="col-lg-2 col-md-4 col-sm-12">
+                        <div className="card">
+                            <img src={burd} alt={burd} className="img-fluid"></img>
+                            <h6 className="text-center" style={{marginTop:'-10px'}}>Fancy Earburds</h6>
+                            <span style={{textAlign:'center', display:'block', color:'#40A578', marginTop:'-5px'}}>This Is Good Product</span>
+                            <span style={{textAlign:'center', display:'block'}}>Rs.1500.00 <span style={{color:'red', textDecoration:'line-through'}}>Rs.5000.00</span></span>
+                        </div>
+                    </div>
+
+
+                    <div className="col-lg-2 col-md-4 col-sm-12">
+                        <div className="card">
+                            <img src={burd} alt={burd} className="img-fluid"></img>
+                            <h6 className="text-center" style={{marginTop:'-10px'}}>Fancy Earburds</h6>
+                            <span style={{textAlign:'center', display:'block', color:'#40A578', marginTop:'-5px'}}>This Is Good Product</span>
+                            <span style={{textAlign:'center', display:'block'}}>Rs.1500.00 <span style={{color:'red', textDecoration:'line-through'}}>Rs.5000.00</span></span>
+                        </div>
+                    </div>
+
+
+                    <div className="col-lg-2 col-md-4 col-sm-12">
+                        <div className="card">
+                            <img src={burd} alt={burd} className="img-fluid"></img>
+                            <h6 className="text-center" style={{marginTop:'-10px'}}>Fancy Earburds</h6>
+                            <span style={{textAlign:'center', display:'block', color:'#40A578', marginTop:'-5px'}}>This Is Good Product</span>
+                            <span style={{textAlign:'center', display:'block'}}>Rs.1500.00 <span style={{color:'red', textDecoration:'line-through'}}>Rs.5000.00</span></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default Boots

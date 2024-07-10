7934e4c68997cd58b348e054d925340903987be2
Rajasree Laha <laharajasree121@gmail.com>
2024-07-10 15:32:00 +0530

    Create App_ass7.js

---
+++ App_ass7.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import './App.css';
+import Division from './components/Division'
+
+function App() {
+  return (
+    <div className="App">
+      <Division/>
+    </div>
+  );
+}
+
+
+export default App;
+

780785d3d61dc53ab3bb4d824dfcd3b9fe86ff26
Rajasree Laha <laharajasree121@gmail.com>
2024-07-10 15:31:03 +0530

    Create Div_chessboard_ass7.jsx

---
+++ Div_chessboard_ass7.jsx
@@ -0,0 +1,144 @@
+import React from "react";
+import './css/Mycss.css';
+
+
+const Division = () =>{
+    return(
+        <>
+            <div className="first">
+
+
+            </div>
+            <div className="sec">
+
+
+            </div>
+            <div className="first">
+
+
+            </div>
+            <div className="sec">
+
+
+            </div>
+            <div className="sec">
+
+
+            </div>
+            <div className="first">
+
+
+            </div>
+            <div className="sec">
+
+
+            </div>
+            <div className="first">
+
+
+            </div>
+            <div className="sirst">
+
+
+            </div>
+            <div className="first">
+
+
+            </div>
+            <div className="sec">
+
+
+            </div>
+            <div className="first">
+
+
+            </div>
+            <div className="sec">
+
+
+            </div>
+            <div className="sec">
+
+
+            </div>
+            <div className="first">
+
+
+            </div>
+            <div className="sec">
+
+
+            </div>
+            <div className="first">
+
+
+            </div>
+            <div className="first">
+
+
+            </div>
+            <div className="sec">
+
+
+            </div>
+            <div className="first">
+
+
+            </div>
+            <div className="sec">
+
+
+            </div>
+            <div className="sec">
+
+
+            </div>
+            <div className="first">
+
+
+            </div>
+            <div className="sec">
+
+
+            </div>
+            <div className="first">
+
+
+            </div><div className="first">
+
+
+            </div>
+            <div className="sec">
+
+
+            </div>
+            <div className="first">
+
+
+            </div>
+            <div className="sec">
+
+
+            </div>
+            <div className="sec">
+
+
+            </div>
+            <div className="first">
+
+
+            </div>
+            <div className="sec">
+
+
+            </div>
+            <div className="first">
+
+
+            </div>
+        </>
+    )
+}
+
+
+export default Division

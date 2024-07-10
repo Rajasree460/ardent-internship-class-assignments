1411dbe6498421af56ff8044e7b65420689dcc37
Rajasree Laha <laharajasree121@gmail.com>
2024-07-10 15:57:19 +0530

    Create Div_sir_ass8.jsx

---
+++ Div_sir_ass8.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import './css/Mycss.css';
+
+
+const Division = () =>{
+    return(
+        <>
+            <div className="first">
+
+            </div>
+            <div className="sec">
+
+
+            </div>
+            <div className="third">
+
+
+            </div>
+            <div className="fourth">
+
+
+            </div>
+            <div className="fifth">
+
+
+            </div>
+        </>
+    )
+}
+
+
+export default Division

38fb63fdf950b4ce095c0c8569240bc51c5753d5
Rajasree Laha <laharajasree121@gmail.com>
2024-07-12 16:07:33 +0530

    Create anym_back_ass3.js

---
+++ anym_back_ass3.js
@@ -0,0 +1,25 @@
+let a=30,b=10;
+
+var add = function(a,b){
+    console.log("add:",a+b)
+}
+
+add(a,b)
+
+var sub = function(a,b){
+    console.log("sub:",a-b)
+}
+
+sub(a,b)
+
+var mul = function(a,b){
+    console.log("mul:",a*b)
+}
+
+mul(a,b)
+
+var div = function(a,b){
+    console.log("div:",a/b)
+}
+
+div(a,b)

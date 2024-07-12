bcad8353c6a528f3cc9b97dec6bdff21c04673cc
Rajasree Laha <laharajasree121@gmail.com>
2024-07-12 15:55:37 +0530

    Create progs_back_ass1.js

---
+++ progs_back_ass1.js
@@ -0,0 +1,22 @@
+let n=17;
+if(n%2==0){
+    console.log("Even");
+}
+else{
+    console.log("Odd");
+}
+
+let n=20;
+if(n>0){
+    console.log("Positive");
+}
+else{
+    console.log("Negative");
+}
+let age=20;
+if(age>=18){
+    console.log("Eligible for voting");
+}
+else{
+    console.log("Not eligible for voting");
+}

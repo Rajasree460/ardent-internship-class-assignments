c4bde9066a50cf5abaee2391a1eb4724ade5579d
Rajasree Laha <laharajasree121@gmail.com>
2024-07-12 15:53:52 +0530

    Create obj_back_ass2.js

---
+++ obj_back_ass2.js
@@ -0,0 +1,35 @@
+var student1 = {
+    name:'Rajasree Laha',
+    age:21,
+    Skill:'Full Stack Developer',
+    status:'Active'
+}
+
+console.log(student1)
+
+var student2 = {
+    name:'abc',
+    age:21,
+    Skill:'Frontend Stack Developer',
+    status:'Active'
+}
+
+console.log(student2)
+
+var emp1 = {
+    name:'aa',
+    age:56,
+    Salary:50000,
+    dept:'IT'
+}
+
+console.log(emp1)
+
+var emp2 = {
+    name:'bb',
+    age:30,
+    Salary:20000,
+    dept:'Sales'
+}
+
+console.log(emp2)

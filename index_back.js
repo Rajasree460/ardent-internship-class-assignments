8693467bf8d90ea6f11145a929a893773400c4f4
Rajasree Laha <laharajasree121@gmail.com>
2024-07-12 15:43:54 +0530

    Create index_back.js

---
+++ index_back.js
@@ -0,0 +1,34 @@
+let string="My name is Rajasree Laha, currently I am in my 4th year of B.Tech in Computer Science Engineering, which I am pursuing from Aliah University, Kolkata ,and my current CGPA is 9.56. I have passed my class 12 with 88.8% and class 10 with 89.28%.I have learnt programming languages like c, Java, python, JavaScript & I am learning web development basically the frontend development. I have hands-on practice in designing a website, making web pages. I have made projects like Landing page, Netflix clone, Tic Tac Toe etc. I did an internship as a web development intern at Oasis Infobyte & heading towards an internship on MERN development at Ardent pvt Lmtd. I am also an opensource contributor.";
+console.log(string);
+
+let a=5;
+let b=6;
+console.log("add(5+6):",a+b);
+
+let a1=5;
+let b1=6;
+console.log("sub(5-6):",a1-b1);
+
+let a2=5;
+let b2=6;
+console.log("Mul(5*6):",a2*b2);
+
+let a3=12;
+let b3=6;
+console.log("div(12/6):",a3/b3);
+
+let base=6,hei=8;
+console.log("triangle area(.5*6*8):",0.5*base*hei);
+
+let r=5;
+console.log("circle area(3.14*5):",3.14*r*r);
+
+let c=5;
+console.log("cube area(6*5*5):",6*c*c);
+
+let l=5,br=6;
+console.log("sq area(5*6):",l*br);
+
+let rd=5;
+console.log("circle perimeter(2*3.14*5):"+2 * 3.14 * rd);
+

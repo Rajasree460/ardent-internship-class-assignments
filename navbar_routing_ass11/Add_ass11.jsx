cfe221a853b954f3210394f77d90dd5faba6d1c1
Rajasree Laha <laharajasree121@gmail.com>
2024-07-15 16:32:08 +0530

    Create Add_ass11.jsx

---
+++ navbar_routing_ass11/Add_ass11.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import Menu from "./Menu";
+
+const Add = () =>{
+    return(
+        <> 
+            <Menu />
+            <h1>Add Page</h1>
+            <form>
+                    <h1>student information form</h1>
+                    <h2>Personal details</h2>
+                    <label>Name</label>
+                    <input type="text" name="name" placeholder="enter name"></input><br></br>
+                    <label>Father Name</label>
+                    <input type="text" name="father name" placeholder="enter father name"></input><br></br>
+                    <label>Mother Name</label>
+                    <input type="text" name="mother name" placeholder="enter mother name"></input><br></br>
+                    <label>Date of birth</label>
+                    <input type="date" name="dob"></input><br></br>
+                    <label>Mobile no.</label>
+                    <input type="number" name="mobile" placeholder="enter mobile no"></input><br></br>
+                    <label>Email Id</label>
+                    <input type="email" name="email" placeholder="enter email id"></input><br></br>
+                    <lable>Image</lable>
+                    <input type="file" name="image" accept=".jpg"></input><br></br>
+
+                    <h2>Address</h2>
+                    <label>Village</label>
+                    <input type="text" name="vill" placeholder="enter village"></input><br></br>
+                    <label>Postoffice</label>
+                    <input type="text" name="post" placeholder="enter postoffice"></input><br></br>
+                    <label>Police St.</label>
+                    <input type="text" name="police" placeholder="enter police station"></input><br></br>
+                    <label>State</label>
+                    <select name="state">
+                    <option disabled selected value={''}>--Select State--</option>
+                    <option value={'West Bengal'}>West Bengal</option>
+                    <option value={'Kerala'}>Kerala</option>
+                    <option value={'Madyhapradesh'}>Madyhapradesh</option>
+                    <option value={'Gujrat'}>gujrat</option>
+                    <option value={'Other'}>Other</option>
+                        </select><br></br>
+                        <label>pin Code</label>
+                        <input type="number" name="pin" placeholder="enter pincode"></input><br></br>
+
+
+                    <h2>Admission</h2>
+                    <label>Class</label>
+                    <select name="class">
+                    <option disabled selected value={''}>--Select class--</option>
+                    <option value={'XII'}>XII</option>
+                    <option value={'XI'}>XI</option>
+                    <option value={'X'}>X</option>
+                    <option value={'Other'}>Other</option>
+                        </select><br></br>
+
+                        <label>Section</label>
+                    <select name="section">
+                    <option disabled selected value={''}>--Select section--</option>
+                    <option value={'A'}>A</option>
+                    <option value={'B'}>B</option>
+                    <option value={'C'}>C</option>
+                        </select><br></br>
+
+                        <lable>CLC</lable>
+                    <input type="file" name="clc" accept=".pdf"></input><br></br>
+                    
+                    <input type="submit" name="ok" value={'Submit'}></input>
+      </form>
+        </>
+    )
+}
+
+export default Add

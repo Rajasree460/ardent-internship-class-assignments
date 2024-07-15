363d5e732d04720a743ec80bc3600e67fceca038
Rajasree Laha <laharajasree121@gmail.com>
2024-07-15 16:58:27 +0530

    Create App_templete_literals.js

---
+++ App_templete_literals.js
@@ -0,0 +1,23 @@
+import React from 'react';
+//import './App.css';
+//import Gatepass from './components/Gatepass';
+
+var a = 10;
+var b = 20;
+var sum = a + b;
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>{`Sum Of Two Number is ${sum}`}</h1>
+    </div>
+  );
+}
+
+
+
+
+export default App;
+
+

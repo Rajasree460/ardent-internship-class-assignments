4894f389cb58fdf5a8619bf474f837bcd82721e9
Rajasree Laha <laharajasree121@gmail.com>
2024-07-15 16:31:37 +0530

    Create About_ass11.jsx

---
+++ navbar_routing_ass11/About_ass11.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Menu from "./Menu";
+
+
+const About = () =>{
+    return(
+        <>
+            <Menu />
+            <h1>About Page</h1>
+            <p>My name is Rajasree Laha.</p>
+        </>
+    )
+}
+
+
+export default About

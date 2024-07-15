3c3477ffdc9d897db879e6b2b8c92f97488833c8
Rajasree Laha <laharajasree121@gmail.com>
2024-07-15 16:33:12 +0530

    Create Contactus_ass11.jsx

---
+++ navbar_routing_ass11/Contactus_ass11.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Menu from "./Menu";
+
+
+const Contactus= () =>{
+    return(
+        <>
+            <Menu />
+            <h1>Contact Page</h1>
+            <h2>Address: 123, ABC Road, XYZ City</h2>
+            <h3>Phone: 1234567890</h3>
+            <h4>Email:abc@gmail.com</h4>
+        </>
+    )
+}
+
+
+export default Contactus

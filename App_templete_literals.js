0dc82a52b614c317bf2ed6380ad9224606a45f2c
Rajasree Laha <laharajasree121@gmail.com>
2024-07-16 16:39:08 +0530

    Update App_templete_literals.js

--- App_templete_literals.js
+++ App_templete_literals.js
@@ -21,3 +21,26 @@ function App() {
 export default App;
 
 
+//another way
+// import React from 'react';
+// //import './App.css';
+// //import Gatepass from './components/Gatepass';
+
+// var a = 10;
+// var b = 20;
+// var sum = a + b;
+// var print=`Sum Of ${a} and ${b} Number is ${sum}`;
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//         <h1>{print}</h1>
+//     </div>
+//   );
+// }
+
+
+
+
+// export default App;

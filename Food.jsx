7e71ed0e926b32684198cbde1ac320a693dcdfd9
Rajasree Laha <laharajasree121@gmail.com>
2024-07-17 16:55:44 +0530

    Create Food.jsx

---
+++ Food.jsx
@@ -0,0 +1,25 @@
+const Food = [
+    {
+        id:1,
+        name:'Sohan',
+        status:'Active'
+    },
+    {
+        id:2,
+        name:'Sohan',
+        status:'Active'
+    },
+    {
+        id:3,
+        name:'Sohan',
+        status:'Active'
+    },
+    {
+        id:4,
+        name:'Sohan',
+        status:'Active'
+    }
+]
+
+
+export default Food

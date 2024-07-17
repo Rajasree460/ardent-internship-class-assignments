05f14470bb5c8164167e78ac7a108a0f38d0c92a
Rajasree Laha <laharajasree121@gmail.com>
2024-07-17 15:38:32 +0530

    Create App_prop.js

---
+++ App_prop.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Prop from './components/Prop';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Prop
+          name = 'Hello Props How r u'
+        />
+    </div>
+  );
+}
+
+
+
+
+export default App;

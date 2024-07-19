bc55df9471d0e1f77759be2557bea8dcad6f53bc
Rajasree Laha <laharajasree121@gmail.com>
2024-07-19 16:10:23 +0530

    Create index_http.js

---
+++ index_http.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+
+const server = http.createServer(function(req,res){
+    res.write("Hello From Node.js Server")
+    res.end()
+})
+
+
+server.listen(8000, () =>{
+    console.log("Server Running On Port Number 8000")
+})

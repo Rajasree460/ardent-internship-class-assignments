c74f758adc1bfd5e3c62f2cb665b94ba606db502
Rajasree Laha <laharajasree121@gmail.com>
2024-07-19 15:37:29 +0530

    Create indexall_back_ass7.js

---
+++ indexall_back_ass7.js
@@ -0,0 +1,26 @@
+const add = (a,b) =>{
+    return a + b;
+}
+
+const sub = (a,b) =>{
+    return a - b;
+}
+
+const mul = (a,b) =>{
+    return a * b;
+}
+
+const div = (a,b) =>{
+    return a / b;
+}
+
+const intro = () =>{
+    let intro="My name is Rajasree Laha";
+    return intro
+}
+
+console.log(add(20,10))
+console.log(sub(20,10))
+console.log(mul(20,10))
+console.log(div(20,10))
+console.log(intro())

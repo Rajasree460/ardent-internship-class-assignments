7fa40def28d668391684859d2c12ead157682aed
Rajasree Laha <laharajasree121@gmail.com>
2024-07-19 17:00:47 +0530

    Create moduleall_back_ass7.js

---
+++ moduleexpall/moduleall_back_ass7.js
@@ -0,0 +1,22 @@
+const add = (a,b) =>{
+    return a + b;
+}
+
+const sub = (a,b) =>{
+    return a - b;
+}
+
+const mul = (a,b) =>{
+    return a * b;
+}
+
+const div = (a,b) =>{
+    return a / b;
+}
+
+const intro = () =>{
+    let intro="My name is Rajasree Laha";
+    return intro
+}
+
+module.exports = {add,sub,mul,div,intro}

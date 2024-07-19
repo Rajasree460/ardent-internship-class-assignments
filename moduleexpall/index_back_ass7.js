d6742b6eead95918e898b5f56afa49a973d2a5e0
Rajasree Laha <laharajasree121@gmail.com>
2024-07-19 17:01:29 +0530

    Create index_back_ass7.js

---
+++ moduleexpall/index_back_ass7.js
@@ -0,0 +1,25 @@
+//ass6
+
+// const add = require('./module1')
+// const sub = require('./module2')
+// const mul = require('./module3')
+// const div = require('./module4')
+// const intro = require('./module5')
+
+
+// console.log(add(20,10))
+// console.log(sub(20,10))
+// console.log(mul(20,10))
+// console.log(div(20,10))
+// console.log(intro())
+
+
+//ass7
+const {add,sub,mul,div,intro} = require('./moduleall')
+
+
+console.log(add(20,10))
+console.log(sub(20,10))
+console.log(mul(20,10))
+console.log(div(20,10))
+console.log(intro())

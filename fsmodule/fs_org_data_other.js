a4723d032a1f83fcce8a48048d3b9b1214382e92
Rajasree Laha <laharajasree121@gmail.com>
2024-08-02 16:02:17 +0530

    Create fs_org_data_other.js

---
+++ fsmodule/fs_org_data_other.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+
+// fs.writeFileSync('gmail.txt', 'Hello From Kolkata')
+
+
+console.log(fs.readFileSync('gmail.txt', 'utf-8'))
+
+
+// const data = fs.readFileSync('gmail.txt')
+// const orgData = data.toString()
+// console.log(orgData);

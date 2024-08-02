dee7a8058bbd9f0716522c61e4939967eeea3266
Rajasree Laha <laharajasree121@gmail.com>
2024-08-02 19:29:28 +0530

    Create even_odd_ass9_back.js

---
+++ repl/even_odd_ass9_back.js
@@ -0,0 +1,17 @@
+function printOddEvenRandom() {
+...     const randomNumber = Math.floor(Math.random() * 100) + 1; // Generates a random number between 1 and 100
+...     if (randomNumber % 2 === 0) {
+...         console.log(`${randomNumber} is even`);
+...     } else {
+...         console.log(`${randomNumber} is odd`);
+...     }
+... }
+undefined
+>
+>
+
+//o/p
+// > printOddEvenRandom();
+// 30 is even
+// undefined
+// >

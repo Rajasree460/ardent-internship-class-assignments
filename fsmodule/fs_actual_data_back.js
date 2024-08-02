b27dc959367546b2cef2b44ea3e0b83aabef612f
Rajasree Laha <laharajasree121@gmail.com>
2024-08-02 16:01:25 +0530

    Create fs_actual_data_back.js

---
+++ fsmodule/fs_actual_data_back.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+
+// fs.writeFileSync('gmail.txt', 'Hello From Kolkata')
+
+
+console.log(fs.readFileSync('gmail.txt'))
+
+
+// const data = fs.readFileSync('gmail.txt')
+// const orgData = data.toString()
+// console.log(orgData);
